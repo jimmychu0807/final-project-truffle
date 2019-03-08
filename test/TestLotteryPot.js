4c0f1fa4672bf89b06ff82744b71c2ba94f1f07b
Jimmy Chu <jimmychu0807@gmail.com>
2019-03-08 19:40:07 +0800

    Added basic test

--- test/TestLotteryPot.js
+++ test/TestLotteryPot.js
@@ -7,6 +7,11 @@ const LotteryPot = artifacts.require("./LotteryPot.sol");
 contract("TestLotteryPot", accts => {
 
   it("should construct a new contract with proper data", async() => {
-    // TODO
+    const lotteryPotData = fixtures.lotteryPots[0];
+
+    const instance = await LotteryPot.deployed();
+    assert.equal(await instance.potName.call(), lotteryPotData.potName);
+    assert.equal((await instance.minStake.call()).toNumber(),
+      lotteryPotData.minStake);
   });
 });
