ad5963bcf57c90f6afe1ed8a015d1db669dd3350
Jimmy Chu <jimmychu0807@gmail.com>
2019-04-01 19:26:58 +0800

    updated deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -9,15 +9,14 @@ const LotteryPotFactory = artifacts.require("LotteryPotFactory");
 module.exports = function(deployer, network, accounts) {
 
   // --- LotteryPot ---
-  // Only deployed in development for testing
-  if (network == "development") {
-    const lotteryPotData = fixtures.lotteryPots.fixtures[0];
-    const minStakeInWei = web3.utils.toWei(lotteryPotData.minStake.toString(), "ether");
+  // We also want to deploy one copy of this contract in rinkeby network.
+  //   So it is recorded in contract artifact and can be read in drizzle.
+  const lotteryPotData = fixtures.lotteryPots.fixtures[0];
+  const minStakeInWei = web3.utils.toWei(lotteryPotData.minStake.toString(), "ether");
 
-    deployer.deploy(LotteryPot, lotteryPotData.potName,
-      w3utils.toBN(lotteryPotData.duration), minStakeInWei,
-      lotteryPotData.potType, accounts[0], { value: minStakeInWei });
-  }
+  deployer.deploy(LotteryPot, lotteryPotData.potName,
+    w3utils.toBN(lotteryPotData.duration), minStakeInWei,
+    lotteryPotData.potType, accounts[0], { value: minStakeInWei });
 
   // --- LotteryPotFactory ---
   deployer.deploy(LotteryPotFactory);
